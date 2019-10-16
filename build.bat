8ddaf20fb0349d4ed5adc6eea575b724faae2f46
andyhou <it.andy.hou@gmail.com>
2019-10-16 15:51:05 +0800

    Update build.bat

--- build.bat
+++ build.bat
@@ -4,6 +4,6 @@ md D:\site
 xcopy site\*.* D:\site /S /F /R /Y /E
 cd D:\Documents\GitHub\it-andy-hou.github.io\
 rd /s /Q site
+start /min cmd /c D:\test.bat
 git checkout master
-xcopy D:\site\*.* D:\Documents\GitHub\it-andy-hou.github.io\  /S /F /R /Y /E
 pause
