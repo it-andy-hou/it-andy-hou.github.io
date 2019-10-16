113462b4e45c2bf075f9a0406f0a1cdbd3e6e912
andyhou <it.andy.hou@gmail.com>
2019-10-16 15:32:36 +0800

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,9 +1,9 @@
 mkdocs build
 del /a /f /s /q D:\site
 md D:\site
-xcopy site\*.* D:\site /s /h /d /y
+xcopy site\*.* D:\site /S /F /R /Y /E
 cd %~dp0
 rd /s /Q site
 git checkout master
-xcopy D:\site\*.* %~dp0
+xcopy D:\site\*.* %~dp0  /S /F /R /Y /E
 pause
