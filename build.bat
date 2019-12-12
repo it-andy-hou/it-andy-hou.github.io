14f4287123156a33c8ed3a59ca7bb3f7dd631d62
Andy Hou <it.andy.hou@gmail.com>
2019-12-12 20:37:44 +0800

    update

--- build.bat
+++ build.bat
@@ -4,10 +4,10 @@ md D:\site
 xcopy site\*.* D:\site /S /F /R /Y /E
 xcopy CNAME D:\site /S /F /R /Y /E
 xcopy README.md D:\site /S /F /R /Y /E
-cd D:\Documents\GitHub\it-andy-hou.github.io\
+cd D:\github\it-andy-hou.github.io\
 rd /s /Q site
 echo ping -n 5 127.0.0.1 ^> nul > D:\test.bat
-echo xcopy D:\site\*.* D:\Documents\GitHub\it-andy-hou.github.io\  /S /F /R /Y /E >> D:\test.bat
+echo xcopy D:\site\*.* D:\github\it-andy-hou.github.io\  /S /F /R /Y /E >> D:\test.bat
 start /min cmd /c D:\test.bat
 git checkout master
 pause
